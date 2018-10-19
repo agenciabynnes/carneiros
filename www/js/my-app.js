c963beb99e94074d33dc40a91cabed097fef2904
Bynnes <falecom@bynn.es>
2018-10-19 05:07:19 -0300

    config ios desenv

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -11774,7 +11774,7 @@ function limpar()
                 },
                 ios: {
                     senderID: "725429976100",
-                    gcmSandbox: "false", // false para producao true para desenvolvimento
+                    gcmSandbox: "true", // false para producao true para desenvolvimento
                     alert: "true",
                     sound: "true",
                     badge: "false"
